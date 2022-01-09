11aaf1bc595127072839cea397e3178d4f026505
die-h <diegohermida94@gmail.com>
2022-01-09 00:24:06 -0300

    Excersice 03 finished

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,18 @@
 const reverseString = function(str) {
+    
+    // let str1 = str.toString();
+    let auxiliar = [];
+    let reversedstr = [];
 
+    for(let i = 0; i < str.length; i++) {
+        auxiliar[i] = str.slice(i, i + 1);
+    }
+
+    for(let i = 0; i < auxiliar.length; i++){
+        reversedstr[i] = auxiliar[auxiliar.length - 1 - i];
+    }
+
+    return reversedstr.join("");
 };
 
 // Do not edit below this line
