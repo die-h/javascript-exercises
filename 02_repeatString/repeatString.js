86efd32f57bf29ad05cdcb1eb2097843efc6900c
die-h <diegohermida94@gmail.com>
2022-01-08 23:40:23 -0300

    1st and 2nd excesices finished

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(chain, cant) {
+    
+    let afterloop = "";
+    
+    if (cant < 0) {
+        return "ERROR";
+    }
+    else {
+        for (i = 0; i < cant; i++) {
+            afterloop = afterloop + chain; 
+        }
+    }
+    return afterloop;
 };
 
 // Do not edit below this line
