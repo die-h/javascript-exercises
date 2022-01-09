dbc2177ae5b22b404d98462cc0f7158122767e60
die-h <diegohermida94@gmail.com>
2022-01-09 02:46:32 -0300

    solved exercise nº5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,34 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+
+    let counter = 0;
+    let sum = 0;
+    let max = 0;
+
+    //Checks which argument is lower to set min and max
+
+    if (num1 < num2) {
+        counter = num1;
+        sum = num1;
+        max = num2;
+    }
+    else {
+        counter = num2;
+        sum = num2;
+        max = num1;
+    }
+    
+    if (sum >= 0 && typeof sum === "number" && typeof max === "number") {
+        while (counter < max) {
+            sum = sum + (counter + 1);
+            counter++;
+        }
+    }
+    else {
+        return "ERROR"
+    }
+    
+
+    return sum;
 
 };
 
